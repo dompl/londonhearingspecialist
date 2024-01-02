e17febf42c9ced3e2d6a6f408e222fd5b525842a
Dom <dom@redfrogstudio.co.uk>
2024-01-02 17:47:32 +0000

    added full image to the dual column

--- wp-content/themes/kickstarter-child/src/components/london-dual-content/acf-fields.php
+++ wp-content/themes/kickstarter-child/src/components/london-dual-content/acf-fields.php
@@ -1,11 +1,12 @@
 <?php
+use Extended\ACF\ConditionalLogic;
 use Extended\ACF\Fields\Group;
 use Extended\ACF\Fields\Image;
+use Extended\ACF\Fields\Layout;
 use Extended\ACF\Fields\Number;
 use Extended\ACF\Fields\Select;
 use Extended\ACF\Fields\Tab;
 use Extended\ACF\Fields\WysiwygEditor;
-// ->conditionalLogic([ConditionalLogic::where('type', '!=', 'document')->and('type', '!=', 'link')]);
 $fields   = [];
 $fields   = array_merge( $fields, \London\Acf::HeaderAcfFields() );
 $fields[] = Tab::make( 'Main Content', wp_unique_id() )->placement( 'left' );
@@ -14,7 +15,12 @@ $fields[] = Tab::make( 'Image', wp_unique_id() )->placement( 'left' );
 $fields[] = Select::make( 'Image position', 'position' )->instructions( 'Set position for the image' )->choices( ['left' => 'Image on the left', 'right' => 'Image on the right'] )->defaultValue( 'right' )->stylisedUi()->required();
 $fields[] = Group::make( 'Image', 'image' )->instructions( 'Set dual content column image' )->fields( [
     Image::make( 'Image', 'image' )->instructions( 'Set the content image' )->returnFormat( 'id' )->previewSize( 'medium' )->required(),
-    Number::make( 'Image height', 'height' )->instructions( 'Set image height' )->min( 100 )->max( 1000 )->required()->defaultValue( 500 ),
+    Number::make( 'Image height', 'height' )->instructions( 'Set image height' )
+        ->min( 100 )->max( 1000 )->required()->defaultValue( 500 )
+        ->conditionalLogic( [ConditionalLogic::where( 'type', '==', 'bcg' )] ),
+    Number::make( 'Image width', 'width' )->instructions( 'Set image width' )
+        ->min( 200 )->max( 900 )->required()->defaultValue( 500 )->step( 10 )
+        ->conditionalLogic( [ConditionalLogic::where( 'type', '==', 'static' )] ),
     Select::make( 'Image type', 'type' )->instructions( 'Select image type' )->choices( ['bcg' => 'Background', 'static' => 'Full image'] )->defaultValue( 'bcg' )->allowNull()->stylisedUi(),
     Select::make( 'Backdrop', 'bcg' )->instructions( 'Add custom backdrop colour for the image as a background' )->choices( london_colors_list() )->allowNull()->stylisedUi()
 ] )->layout( 'row' );
--- wp-content/themes/kickstarter-child/src/components/london-dual-content/html.php
+++ wp-content/themes/kickstarter-child/src/components/london-dual-content/html.php
@@ -7,23 +7,27 @@ function wp_1700569337_london( $html, $data ) {
 
     $image    = get_component( 'image', $data, 'image' );
     $height   = get_component( 'image', $data, 'height' );
+    $width    = get_component( 'image', $data, 'width' ) ? get_component( 'image', $data, 'width' ) : 500;
     $backdrop = get_component( 'image', $data, 'bcg' );
     $content  = get_component( 'content', $data );
     $position = get_component( 'position', $data );
+    $type     = get_component( 'image', $data, 'type' ) ? get_component( 'image', $data, 'type' ) : 'bcg';
 
     $right = '';
     $left  = '<div class="pos-left">';
     $left .= Acf::HeaderAcfHtml( $data );
     $left .= $content ? '<div class="london-content">' . wpautop( $content ) . '</div>' : '';
     $left .= Acf::ButtonAcfHtml( $data, 's' );
-    $left .= empty( $backdrop ) ? '' : '<div class="backdrop b-pos-' . $position . ' bcg-' . $backdrop . '"></div>';
+    $left .= empty( $backdrop ) ? '' : '<div class="backdrop b-pos-' . $position . ' bcg-' . $backdrop . ' int-' . $type . '"></div>';
     $left .= '</div>';
 
     if ( $image ) {
 
         $id = 'london-dual-' . $data['post_id'] . $data['index'];
 
-        $css = "#{$id} {
+        if ( $type == 'bcg' ) {
+
+            $css = "#{$id} {
 			background-image: url(" . MyHelpers::WPImage( id: $image, size: [300, $height], retina: false ) . ");
 			@media
 				only screen and (-webkit-min-device-pixel-ratio: 2),
@@ -37,9 +41,16 @@ function wp_1700569337_london( $html, $data ) {
 
 			}";
 
-        MyHelpers::AddCustomCss( $css );
+            MyHelpers::AddCustomCss( $css );
 
-        $right = '<div id="' . $id . '" class="pos-right" style="background-image:url(' . MyHelpers::WPImage( id: $image, size: [520, $height], retina: false ) . ')"></div>';
+            $right = '<div id="' . $id . '" class="pos-right" style="background-image:url(' . MyHelpers::WPImage( id: $image, size: [520, $height], retina: false ) . ')"></div>';
+
+        } else {
+
+            $img   = MyHelpers::PictureSource( image: $image, size: $width, min: 480, custom_container: 'image' );
+            $right = '<div class="pos-right ' . $type . '">' . $img . '</div>';
+
+        }
 
     }
 
