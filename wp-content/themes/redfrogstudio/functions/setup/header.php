d5237882d2251d6c37c7efa32b2ca9f8eec42f31
Dom <dom@redfrogstudio.co.uk>
2024-02-16 09:52:07 +0000

    fixed header

--- wp-content/themes/redfrogstudio/functions/setup/header.php
+++ wp-content/themes/redfrogstudio/functions/setup/header.php
@@ -13,8 +13,16 @@ function ks_add_favicon() {
     if ( isset( $data['ks_favicon'] ) ) {
         $image_id   = $data['ks_favicon'];
         $image_data = wp_get_attachment_metadata( $image_id );
-        if ( $image_data ) {
-            echo '<link rel="icon" type="' . $image_data['sizes']['mini-thumbnail']['mime-type'] . '" href="' . MyHelpers::WPImage( id: $image_id, size: [32, 32], webp: null ) . '">' . "\n";
+        $image_path = wp_get_attachment_url( $image_id ); // Get the URL of the original image
+
+        // Use the mime type of the original image
+        $mime_type = isset( $image_data['mime-type'] ) ? $image_data['mime-type'] : 'image/png'; // Default to image/png if not found
+
+        // Since 'sizes' is empty, use the original image for the favicon
+        if ( $image_path ) {
+            echo '<link rel="icon" type="' . $mime_type . '" href="' . $image_path . '">' . "\n";
         }
     }
 }
+
+add_action( 'wp_head', 'ks_add_favicon' );
