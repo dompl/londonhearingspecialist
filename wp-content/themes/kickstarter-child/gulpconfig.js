dbd1c08f67587a95b82e7b38c32ae2796ba98833
Dom <dom@redfrogstudio.co.uk>
2024-02-19 16:08:55 +0000

    Increment theme version in gulpconfig.js to 1.0.21

--- wp-content/themes/kickstarter-child/gulpconfig.js
+++ wp-content/themes/kickstarter-child/gulpconfig.js
@@ -4,7 +4,7 @@ module.exports = {
     "description": "Child theme for the Kickstarter/Red Frog Studio",
     "name": "London Hearing Specialist",
     "url": "http://londonhearingspecialist.co.uk",
-    "version": "1.0.20"
+    "version": "1.0.21"
   },
   "project": {
     "name": "londonhearingspecialist",
