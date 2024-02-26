9f02616a9fa23601541a953aa439510ca3b63349
Dom <dom@redfrogstudio.co.uk>
2024-02-26 10:08:21 +0000

    Development in progress

--- wp-content/themes/kickstarter-child/src/components/london-thanks/acf-fields.php
+++ wp-content/themes/kickstarter-child/src/components/london-thanks/acf-fields.php
@@ -5,7 +5,7 @@ use Extended\ACF\Fields\WysiwygEditor;
 $fields          = [];
 $default_message = '<p>Your enquiry has been successfully sent. We will review this shortly to get back to you to organise an appointment or reply to your query.</p>';
 $default_message .= '<p>Should you have an emergency appointment requirement do give us a call to speak to one of our professionals.</p>';
-$fields[] = Tab::make( 'Content', wp_unique_id() )->placement( 'left' );
+$fields[] = Tab::make( 'Content', wp_unique_id( 'thank_you_content' ) )->placement( 'left' );
 $fields[] = Text::make( 'Title', 'title' )->instructions( 'Add thank you message title' )->defaultValue( 'Thank you!' );
 $fields[] = WysiwygEditor::make( 'Message', 'message' )->instructions( 'Add thank you message text' )->defaultValue( $default_message )->mediaUpload( false )->tabs( 'all' )->toolbar( 'default_toolbar' )->required();
 \Kickstarter\MyAcf::registerComponentFields( 'Thank you', $fields );
