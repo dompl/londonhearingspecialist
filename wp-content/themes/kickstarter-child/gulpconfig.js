1f21d3b064dc5fcdefac9d55215feb7a187cf740
rfsdev server <ino@redfrogstudio.co.uk>
2024-11-26 13:10:16 +0000

    Increment theme version to 1.0.55

--- wp-content/themes/kickstarter-child/gulpconfig.js
+++ wp-content/themes/kickstarter-child/gulpconfig.js
@@ -4,7 +4,7 @@ module.exports = {
     "description": "Child theme for the Kickstarter/Red Frog Studio",
     "name": "London Hearing Specialist",
     "url": "http://londonhearingspecialist.co.uk",
-    "version": "1.0.54"
+    "version": "1.0.55"
   },
   "project": {
     "name": "londonhearingspecialist",
